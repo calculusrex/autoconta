082ec8b8c1dfe59437077f87f33d775203818b63
Zorilă Mircea <zorila.m.w@gmail.com>
2022-06-27 19:50:51 +0300

    new pipeline control shift architecture

--- document_preprocessing/batching_workflow.py
+++ document_preprocessing/batching_workflow.py
@@ -64,82 +64,152 @@ images_data = list(map(read_im, full_filenames[:2]))
 # --------------------------------
 # WARPING
 
-def destruction_procedure(frame, pipeline_data):
-    # display_cv(frame.proc_im)
-    progress_data = {
-        'param_data': frame.param_data,
-        'processed_image': frame.proc_im,
-        'original_image_fname': frame.og_im_fname,
-    }
-    pipeline_data['progress_sequence'].append(
-        progress_data)
-    parent = frame.master
-    frame.destroy()
-    return parent, pipeline_data
+# def destruction_procedure(frame, pipeline_data):
+#     # display_cv(frame.proc_im)
+#     progress_data = {
+#         'param_data': frame.param_data,
+#         'processed_image': frame.proc_im,
+#         'original_image_fname': frame.og_im_fname,
+#     }
+#     pipeline_data['progress_sequence'].append(
+#         progress_data)
+#     parent = frame.master
+#     frame.destroy()
+#     return parent, pipeline_data
 
-def construction_procedure(parent, pipeline_data):
-    if len(pipeline_data['construction_sequence']) == 0 and len(pipeline_data['pipeline_constructor_sequence']) == 0:
-        for document_data in pipeline_data['progress_sequence']:
-            out_fpath = "/".join([
-                OUTPUT_FOLDER,
-                ".".join([
-                    document_data['original_image_fname'].split(
-                        ".")[0],
-                    "png"])])
-            cv.imwrite(
-                out_fpath,
-                document_data['processed_image'])
-            print(f"written: {out_fpath}")
+# def construction_procedure(parent, pipeline_data):
+#     if len(pipeline_data['construction_sequence']) == 0 and len(pipeline_data['pipeline_constructor_sequence']) == 0:
+#         for document_data in pipeline_data['progress_sequence']:
+#             out_fpath = "/".join([
+#                 OUTPUT_FOLDER,
+#                 ".".join([
+#                     document_data['original_image_fname'].split(
+#                         ".")[0],
+#                     "png"])])
+#             cv.imwrite(
+#                 out_fpath,
+#                 document_data['processed_image'])
+#             print(f"written: {out_fpath}")
 
-    else:
-        if len(pipeline_data['pipeline_constructor_sequence']) == 0:
-            pipeline_sequence_data = pipeline_data['construction_sequence'].pop()
-            pipeline_data['pipeline_constructor_sequence'] = pipeline_sequence_data[
-                'pipeline_constructors']
-            image_data = current_sequence_data['image_data']
-            pipeline_data['current_original_image_data'] = image_data
-            pipeline_data['current_improc_progress'] = []
+#     else:
+#         if len(pipeline_data['pipeline_constructor_sequence']) == 0:
+#             pipeline_sequence_data = pipeline_data['construction_sequence'].pop()
+#             pipeline_data['pipeline_constructor_sequence'] = pipeline_sequence_data[
+#                 'pipeline_constructors']
+#             image_data = current_sequence_data['image_data']
+#             pipeline_data['current_original_image_data'] = image_data
+#             pipeline_data['current_improc_progress'] = []
 
-        image_data = pipeline_data['current_improc_progress'][-1]
-        constructor = pipeline_data['pipeline_constructor_sequence'].pop()
-        frame = constructor(
-            parent, image_data, pipeline_data, data_directed=True)
-        frame.grid(
-            row=0, column=0, rowspan=FRAME_ROWSPAN)
+#         image_data = pipeline_data['current_improc_progress'][-1]
+#         constructor = pipeline_data['pipeline_constructor_sequence'].pop()
+#         frame = constructor(
+#             parent, image_data, pipeline_data, data_directed=True)
+#         frame.grid(
+#             row=0, column=0, rowspan=FRAME_ROWSPAN)
         
         
 
-frame_constructors =  [
-        WarpingEditor, OrthogonalRotationEditor,
-        FineRotationEditor, RescaleEditor, CropEditor
-]
-frame_constructors.reverse()
-
-pipeline_data = {
-    'frame_constructors': frame_constructors,
-    'construction_sequence': []
-    'progress_sequence': [],
-    'construction_procedure': construction_procedure,
-    'destruction_procedure': destruction_procedure,
-}
-
-for image_data in images_data:
-    pipeline_data['construction_sequence'].append(
-        pipeline_data.append({
-            'image_data': image_data,
-            'pipeline_constructors': frame_constructors.copy()
-        }))
+# frame_constructors =  [
+#         WarpingEditor, OrthogonalRotationEditor,
+#         FineRotationEditor, RescaleEditor, CropEditor
+# ]
+# frame_constructors.reverse()
 
 # pipeline_data = {
-#     'frame_constructor': WarpingEditor,
-#     'construction_sequence': images_data,
+#     'frame_constructors': frame_constructors,
+#     'construction_sequence': []
 #     'progress_sequence': [],
 #     'construction_procedure': construction_procedure,
 #     'destruction_procedure': destruction_procedure,
 # }
 
+# for image_data in images_data:
+#     pipeline_data['construction_sequence'].append(
+#         pipeline_data.append({
+#             'image_data': image_data,
+#             'pipeline_constructors': frame_constructors.copy()
+#         }))
+
+# # pipeline_data = {
+# #     'frame_constructor': WarpingEditor,
+# #     'construction_sequence': images_data,
+# #     'progress_sequence': [],
+# #     'construction_procedure': construction_procedure,
+# #     'destruction_procedure': destruction_procedure,
+# # }
+
+preproc__constructor_from_stage_name = dict(
+    zip(['warping', 'orthogonal_rotation',
+         'fine_rotation', 'rescale', 'crop'],
+        [WarpingEditor, OrthogonalRotationEditor,
+         FineRotationEditor, RescaleEditor, CropEditor]))
+
+# filename -> preproc_im
+# preproc_im :: (fname, proc_im, [(proc_name, proc_params)])
+
+preproc_stages = [
+    'warping' , 'orthogonal_rotation', 'fine_rotation', 'rescale', 'crop']
+
+def im__transform_pipeline_data__from__filename(
+        fname, fpath):
+    extension = fname.split('.')[-1]
+    fname_wo_extension = "".join(
+        fname.split('.')[:-1])
+    im = read_im(fpath)
+    return {
+        'fname': fname, 'fpath': fpath,
+        'fname_wo_extension': fname_wo_extension,
+        'im': im,
+        'pending_procs': preproc_stages.copy(),
+        'finished_proc_data': []
+    }
+
+
+def transform_pipeline__destructor(frame):
+    transform_pipeline_data = frame.transform_pipeline_data
+    transform_pipeline_data['finished_proc_data'].append(
+        {'proc_name': frame.proc_name
+         'proc_params': frame.param_data})
+    gui_data = {'root': frame.master}
+    frame.destroy()
+    return transform_pipeline_data, gui_data
+
+def transform_pipeline__constructor(
+        transform_pipeline_data, gui_data):
+    frame_constructor = preproc__constructor_from_stage_name[
+        transform_pipeline_data[
+            'pending_procs'].pop()]
+    frame = frame_constructor(
+        transform_pipeline_data, gui_data)
+    frame.grid(
+        row=0, column=0, rowspan=FRAME_ROWSPAN)    
+
+def transform_pipeline__control_shift(frame):
+    dest = transform_pipeline__destructor
+    transform_pipeline_data, gui_data = dest(
+        frame)
+    if transform_pipeline_data['pending_procs']: # not empty
+        transform_pipeline__constructor(
+            transform_pipeline_data, gui_data)
+    else:
+        preproc_im_data = transform_pipeline__collector(
+            transform_pipeline_data, gui_data)
+        
+        
+    
+
+
+def transform_pipeline__collector(
+        transform_pipeline_data, gui_data):
+    pass
+
+def transform_pipeline__progressor(
+        pipeline_data, gui_data):
+    pass
+
+
 
 root = tk.Tk()
 root.bind('<Control-q>', lambda event: root.destroy())
-construction_procedure(root, pipeline_data)
+# construction_procedure(root, pipeline_data)
 
